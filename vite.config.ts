d461bf9797d2294e1083e3edaf27f724ee51fd6d
Martin Le <minh.le023@gmail.com>
2023-05-07 15:11:03 +1000

    Enable full page reloads by path (#2)

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import FullReload from "vite-plugin-full-reload";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
         },
       },
     }),
+    FullReload(["src/components/widgets/*"]),
   ],
   // prevent vite from obscuring rust errors
   clearScreen: false,
